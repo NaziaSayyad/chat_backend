893dad3a69d363afd910dc308da72279f1cfb8c4
Nazia Sayyad <sayyadnazia257@gmai.com>
2023-03-12 17:19:04 +0530

    some-updates

--- controllers/userController.js
+++ controllers/userController.js
@@ -54,17 +54,16 @@ const authuser = async(req,res) =>{
 
 const alluser = async (req,res) => {
       const keyword = req.query.search
-        ? {
-            $or: [
-              { name: { $regex: req.query.search, $options: "i" } },
-              { email: { $regex: req.query.search, $options: "i" } },
-            ],
-          }
-        : {};
-
-      const users = await User.find(keyword).find(
-        { _id: { $ne: req.user._id } });
-      res.send(users);
+      ? {
+          $or: [
+            { name: { $regex: req.query.search, $options: "i" } },
+            { email: { $regex: req.query.search, $options: "i" } },
+          ],
+        }
+      : {};
+    // res.send(keyword);
+    const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+    res.send(users);
 }
 
 module.exports = { registeruser, authuser, alluser };
